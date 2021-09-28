e4de20e1bd10beaaf886b6c927d0aa3ea4feaf7b
Andres Pena <penchjpc@gmail.com>
2021-09-28 09:46:00 -0400

    JS:created api key and city,zipcode variables; HTML: added google fonts;CSS: added google font-family

--- script.js
+++ script.js
@@ -2,5 +2,10 @@
 let searchBtn = document.getElementById('search-btn')
 
 // Create variables
+let apiKey = 'bd9db36f5d13b809001a47d45f105dd0'
+let city
+let zipCode
 
 // Create and append history button
+
+
